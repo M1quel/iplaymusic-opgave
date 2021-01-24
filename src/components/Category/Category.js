af5329c9f0236e7767e4abd161731ebcc1b13309
Mikkel Frederiksen <5760459@rts-365.dk>
2021-01-24 19:23:11 +0100

    update

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,19 +1,22 @@
 import "./Category.css";
 
 function Category (props) {
-
+    var labelStyling = {
+        "background-color": props.bgcolor
+    }
     var underCats = props.underCats;
     return(
-        <article>
-            <label>
-                <h1>HEADING</h1>
+        <article className="category">
+            <label style={ labelStyling } className="categoryLabel" htmlFor="check">
+                <h1 className="categoryHeading">{props.heading}</h1>
                 <div className="label">
                     <span></span>
                     <span></span>
                     <span></span>
                 </div>
             </label>
-            <ul>
+            <input type="checkbox" name="check" id="check"/>
+            <ul className="underCatList">
                 {underCats.map(function(cat) {
 				    return <li> { cat } </li>
 			    })}
